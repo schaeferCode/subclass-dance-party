876b9bbc8ffbcf0890a686403611b1510eef46b0
Scott Schaefer <scottschaef@gmail.com>
2017-07-10 21:49:16 -0400

    Set prototype to Dancer.prototype and set contructor to BlinkDancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,15 +1,18 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
 
-  // call the old version of step at the beginning of any call to this new version of step
   var oldStep = Dancer.step;
 
 };
 
-  BlinkyDancer.prototype.step = function() {
-    this.oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    this.$node.toggle();
-  };
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.step = function() {
+  this.oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
+
